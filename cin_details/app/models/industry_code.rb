86d2990c406c5a07628ece90d88bf59c17683997
Suneel.Mogilipuri <suneel.mogilipuri@acsicorp.com>
2020-12-19 16:59:37 +0530

    removed byebug

--- cin_details/app/models/industry_code.rb
+++ cin_details/app/models/industry_code.rb
@@ -1,6 +1,5 @@
 class IndustryCode < ApplicationRecord
     def self.get_industry_code_description(cin_number)
-        byebug
         if ind_code = self.find_by_code(cin_number[1,5]) 
             ind_code.description 
         else
